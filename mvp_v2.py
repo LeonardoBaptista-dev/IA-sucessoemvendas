288ddd29323c7a5496111496b7b26f773bfd3876
Leonardo Baptista <111295521+LeonardoBaptista-dev@users.noreply.github.com>
2024-09-20 10:39:09 -0300

    Update mvp_v2.py

--- mvp_v2.py
+++ mvp_v2.py
@@ -4,7 +4,8 @@ import streamlit as st
 st.set_page_config(page_title='Consultor da Sucesso em Vendas', layout="wide")
 
 # Resto das importações
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleAuthProvider
+from langchain_google_genai import GoogleAuthProvider
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import json
